877090dc685ba2559e79e4b67299b4cbb77e894b
Justin Wykstra <justinwykstra@gmail.com>
2016-11-04 13:32:00 -0600

    Assigned multiple data types

---
+++ 009_union_types.js
@@ -0,0 +1,10 @@
+var players = ["Altuve", "Corerra", "Bregman"];
+var player_numbers = [25, 3, 2];
+console.log(players);
+console.log(player_numbers);
+var names;
+names = ["Justin Wykstra", "Laura Hauck"];
+console.log(names);
+names = "Jack Reacher";
+console.log(names);
+//# sourceMappingURL=009_union_types.js.map
---
+++ 009_union_types.ts
@@ -0,0 +1,11 @@
+type PlayerArray = Array<string|number>; 
+let players : PlayerArray = ["Altuve", "Corerra", "Bregman"];
+let player_numbers : PlayerArray = [25, 3, 2];
+console.log(players);
+console.log(player_numbers);
+
+var names : string[]|string; 
+names = ["Justin Wykstra", "Laura Hauck"];
+console.log(names);
+names = "Jack Reacher";
+console.log(names);

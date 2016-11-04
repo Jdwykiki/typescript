68343dc89827243df9352b4619e8b8b2b2a747fd
Justin Wykstra <justinwykstra@gmail.com>
2016-11-04 13:39:46 -0600

    Compound conditionals in TypeScript programs

---
+++ 013_compound_conditionals.js
@@ -0,0 +1,11 @@
+var password = 'asdfasdf';
+var email = 'test@test.com';
+// if (password == 'asdfasdf' || password == 'qwerqwer') {
+// 	console.log('Password is correct');
+// } else {
+// 	console.log('Permission denied');
+// }
+if (!(email == 'test@test.com')) {
+    console.log('You are authorized');
+}
+//# sourceMappingURL=013_compound_conditionals.js.map
---
+++ 013_compound_conditionals.ts
@@ -0,0 +1,12 @@
+let password : string = 'asdfasdf';
+let email : string = 'test@test.com';
+
+// if (password == 'asdfasdf' || password == 'qwerqwer') {
+// 	console.log('Password is correct');
+// } else {
+// 	console.log('Permission denied');
+// }
+
+if (!(email == 'test@test.com')) {
+	console.log('You are authorized');
+}
